1989aba70b6d008fff388679aae5d4167832e5ce
yashbakliwal01 <yashbakliwal01@gmail.com>
2026-01-13 19:13:10 +0530

    initialized project

---
+++ src/test/java/com/ai/knowRAG/KnowRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.knowRAG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
